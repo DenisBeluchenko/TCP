1e40fc1b25ed6b2a2c9f9acfc8d247e0c738642d
Denis Beluchenko <dzibis@gmail.com>
2022-12-26 22:13:03 +0700

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,33 @@
+import jdk.internal.org.objectweb.asm.ClassReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String city="???";
+        try (ServerSocket serverSocket = new ServerSocket(Config.PORT);) {
+            System.out.println("Сервер запущен");
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                ) {
+                    System.out.println("подключен порт->" + socket.getPort());
+                    Scanner scanner = new Scanner(System.in);
+                    out.println(scanner.nextLine());
+                    System.out.println(in.readLine());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
